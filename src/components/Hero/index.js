f26def5b3aa6df1a772a061a7c05fb8a3f9c97d8
Romano78 <rom.penaruiz@gmail.com>
2025-09-09 15:42:58 -0400

    basic config nav and hero

---
+++ src/components/Hero/index.js
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className="container bg-background">
+            <h1 className="text-5xl font-bold">Hero</h1>
+        </div>
+    );
+};
+
+
+export default Hero;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <section className="container">
+            <div className="flex justify-between">
+                <h1>Romain Pena Ruiz</h1>
+            </div>
+        </section>
+    );
+};
+
+
+export default Nav;
